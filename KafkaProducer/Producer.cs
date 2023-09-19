9d63e8a7bfbc7db9ab8dd08384e499dcec896b03
Egor Shevdin <egor_sh@live.de>
2023-09-19 11:44:43 +0200

    - use string message but serialize as json in producer

--- KafkaProducer/Producer.cs
+++ KafkaProducer/Producer.cs
@@ -3,6 +3,7 @@ using CoreLibrary;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Text.Json;
 
 namespace KafkaProducer
 {
@@ -10,14 +11,14 @@ namespace KafkaProducer
     {
         private readonly ILogger _logger;
         private readonly string _topic;
-        private readonly IProducer<Null, RandomMessage> _producer;
+        private readonly IProducer<Null, string> _producer;
 
         public Producer(ILogger<Producer> logger, IOptions<KafkaOptions> kafkaOptions) 
         {
             _logger = logger;
             _topic = kafkaOptions.Value.Topic;
 
-            _producer = new ProducerBuilder<Null, RandomMessage>(
+            _producer = new ProducerBuilder<Null, string>(
                 new ProducerConfig { BootstrapServers = kafkaOptions.Value.BootstrapUrl })
                 .Build();
 
@@ -29,7 +30,8 @@ namespace KafkaProducer
             while (!stoppingToken.IsCancellationRequested)
             {
                 var randomNumber = Random.Shared.Next(100);
-                _producer.Produce(_topic, new Message<Null, RandomMessage> { Value = new RandomMessage { Id = randomNumber, Message = "Message produced" } });
+                var message = JsonSerializer.Serialize(new RandomMessage { Id = randomNumber, Message = "Message produced." });
+                _producer.Produce(_topic, new Message<Null, string> { Value = message });
                 _logger.LogInformation("Produced message with number {randomNumber}.", randomNumber);
                 await Task.Delay(1000, stoppingToken);
             }
